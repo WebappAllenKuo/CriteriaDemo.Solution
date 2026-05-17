12f0162425e7c84e13f58eed40e03440a795f768
allen <g01.webapp@gmail.com>
2026-05-17 13:00:25 +0800

    第一版成品

--- CriteriaDemo.WinApp/Form1.cs
+++ CriteriaDemo.WinApp/Form1.cs
@@ -17,6 +17,45 @@ namespace CriteriaDemo.WinApp
 			GenerateSQL();
 		}
 
+		private string GetStatusFilter()
+		{
+			if (s_Status.SelectedIndex < 0) return string.Empty;
+
+			var status = s_Status.SelectedItem.ToString();
+			return status;
+		}
+
+		private string GetStartDateFilter()
+		{
+			if(string.IsNullOrEmpty(s_StartTime.Text)) return string.Empty;
+
+			bool isValid = DateTime.TryParse(s_StartTime.Text, out var startTime);
+			if (isValid)
+			{
+				return startTime.ToString("yyyy/MM/dd");
+			}
+			else
+			{
+				return string.Empty;
+			}
+		}
+
+		private string GetEndDateFilter()
+		{
+			if (string.IsNullOrEmpty(s_EndTime.Text)) return string.Empty;
+
+			bool isValid = DateTime.TryParse(s_EndTime.Text, out var endTime);
+
+			if (isValid)
+			{
+				return endTime.ToString("yyyy/MM/dd");
+			}
+			else
+			{
+				return string.Empty;
+			}
+		}
+
 		private void GenerateSQL()
 		{
 			var sql = @"SELECT
@@ -31,10 +70,28 @@ INNER JOIN Members m ON o.MemberId = m.Id";
 			// Build the WHERE clause based on user input
 			var where = string.Empty;
 
+			var s_status = GetStatusFilter();
+			if (string.IsNullOrEmpty(s_status) == false)
+			{
+				where += $" AND o.Status = '{s_status}'";
+			}
+
+			var s_startTime = GetStartDateFilter();
+			if (string.IsNullOrEmpty(s_startTime) == false)
+			{
+				where += $" AND o.OrderTime >= '{s_startTime}'";
+			}
+
+			var s_endTime = GetEndDateFilter();
+			if (string.IsNullOrEmpty(s_endTime) == false)
+			{
+				var dtEnd = DateTime.Parse(s_endTime).AddDays(1).ToString("yyyy/MM/dd");
+				where += $" AND o.OrderTime < '{dtEnd}'";
+			}
 
 			// 若有任一篩選條件，則將其加入 WHERE 子句
 			if (string.IsNullOrEmpty(where) == false) { 
-				sql += "\r\nWHERE " + where;
+				sql += "\r\nWHERE " + where.Substring(5);
 			}
 
 			sql += "\r\nORDER BY o.OrderTime DESC";
